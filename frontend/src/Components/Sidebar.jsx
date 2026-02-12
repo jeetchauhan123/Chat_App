214c18851e33618fb8bae0d29369fd99b2577fdf
Jeet Chauhan <spgtjeet123@gmail.com>
2026-02-12 17:28:15 +0530

    Working on Chat Page

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,16 +1,57 @@
 import React from "react";
 
 const Sidebar = ({ user }) => {
-  console.log(user);
+  console.log(user.contacts);
   return (
-    <div className="w-[20vw] h-full p-5 flex flex-col gap-5 bg-amber-200 rounded-2xl">
+    <div className="w-[20vw] h-full  flex flex-col gap-5 bg-[#201919] rounded-2xl overflow-auto scroll-auto">
       <header>
-        <h1 className="text-2xl">{user?.name}</h1>
+        <h1 className="text-2xl text-white">{user?.name}</h1>
       </header>
 
       {/* chats contacts */}
-      
-      <main className="flex flex-col gap-4">
+      <main className="flex flex-col ">
+        {user?.contacts?.map((c, index) => (
+          <section
+            key={index}
+            className="flex flex-col rounded-xl"
+          >
+            <div className="px-5 py-6 rounded-xl cursor-pointer hover:bg-[#373131] transition">
+              <div className="flex flex-row justify-between items-center text-white">
+                {/* name */}
+                <span className="text-lg font-semibold">{c.name}</span>
+
+                {/* time */}
+                <span className="text-[#acacac] text-xs ">
+                  {new Date(c.lastMessageTime).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </span>
+              </div>
+
+              {/* last message */}
+              <p className="truncate  text-[#acacac] ">{c.lastMessage}</p>
+            </div>
+            <hr className="text-[#575454] mx-4" />
+          </section>
+        ))}
+        {user?.contacts?.map((c, index) => (
+          <section
+            key={index}
+            className=""
+          >
+            <div className="">
+              {/* name */}
+              <span className="">{c.name}</span>
+
+              {/* time */}
+              <span className="text-xs text-gray-500"></span>
+            </div>
+
+            {/* last message */}
+            <p className="text-sm text-gray-600 truncate">{c.lastMessage}</p>
+          </section>
+        ))}
         {user?.contacts?.map((c, index) => (
           <section
             key={index}
@@ -21,12 +62,7 @@ const Sidebar = ({ user }) => {
               <span className="font-semibold">{c.name}</span>
 
               {/* time */}
-              <span className="text-xs text-gray-500">
-                {new Date(c.lastMessageTime).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </span>
+              <span className="text-xs text-gray-500"></span>
             </div>
 
             {/* last message */}
@@ -34,6 +70,7 @@ const Sidebar = ({ user }) => {
           </section>
         ))}
       </main>
+      <main className="flex flex-col gap-4"></main>
     </div>
   );
 };
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 import { useLocation } from "react-router-dom";
-import Sidebar from '../Components/Sidebar'
+import Sidebar from "../Components/Sidebar";
+import users from "../data/users.json";
 
 const Chat = () => {
   const location = useLocation();
-  const user = location.state?.user;
-  // console.log(user)
-  return (
-    <section className='h-screen p-5'>
-      <Sidebar user={user}/>
-    </section>
-  )
-}
+  const loggedUserName = location.state?.user?.name;
 
-export default Chat
+  const user = users.find((u) => u.name === loggedUserName);
+
+  if (!user) {
+    return <p className="p-5">User not found</p>;
+  }
+
+  console.log("chat", user);
+  return (
+    <section className="h-screen p-5">
+      <img src="/chat_bg4.jpg" alt="bg_img" className="absolute object-cover top-0 left-0 -z-10" />
+      <Sidebar user={user} />
+    </section>
+  );
+};
+
+export default Chat;
