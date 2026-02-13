2729902c7919fdc241628c6aeb536392a360db31
Jeet Chauhan <spgtjeet123@gmail.com>
2026-02-13 13:25:36 +0530

    Making a Chat Panel

--- frontend/src/Components/ChatNav.jsx
+++ frontend/src/Components/ChatNav.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 const ChatNav = (user) => {
-    console.log("chatnav",user)
+  console.log("chatnav", user);
   return (
-    <section className="p-4  bg-[#494949] rounded-t-2xl">
-      <div className="flex items-center gap-3">
+    <section className="px-6 py-4 flex justify-between bg-[#434343] rounded-t-2xl">
+      <div className="flex items-center gap-4">
         <span className="w-10 h-10 p-4 flex items-center justify-center font-bold rounded-full bg-[#9e9e9e]">
           {user?.user?.name?.charAt(0).toUpperCase()}
         </span>
-        <h1 className="text-2xl">{user.user.name}</h1>
+        <h1 className="text-2xl font-semibold">{user.user.name}</h1>
+      </div>
+
+      <div className="h-10 w-10 hover:bg-[#9e9e9e89] p-1 rounded-full transition">
+        <img src="/three-dots-menu.svg" alt="menu" className="" />
       </div>
     </section>
   );
--- frontend/src/Components/ChatPanel.jsx
+++ frontend/src/Components/ChatPanel.jsx
@@ -4,7 +4,7 @@ import ChatNav from './ChatNav'
 const ChatPanel = (user) => {
     console.log("userpanel",user);
   return (
-    <div className='h-full w-full bg-[#2f2f2f] rounded-2xl'>
+    <div className='h-full w-full bg-[#272727] rounded-2xl'>
         <ChatNav user={user.user}/>
     </div>
   )
